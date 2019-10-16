2fed3fe59df79301b6651285e418a61ccc197cce
Jonata Souza Cunha <jonatasouzac@hotmail.com>
2019-10-16 16:12:15 -0300

    Using pipfile

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import unittest
-
+import redis
+redis_db = redis.StrictRedis(host="localhost", port=6379, charset="utf-8", decode_responses=True, db=0)
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(False, True)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
